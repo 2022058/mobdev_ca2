5052f616af035872feb59a997032a4905343c66c
2022058 <2022058@student.cct.ie>
2022-11-04 15:51:13 +0000

    Add starships

--- src/app/pages/starships/starships-routing.module.ts
+++ src/app/pages/starships/starships-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { PlanetsPage } from './planets.page';
+import { StarshipsPage } from './starships.page';
 
 const routes: Routes = [
   {
     path: '',
-    component: PlanetsPage
+    component: StarshipsPage
   }
 ];
 
@@ -14,4 +14,4 @@ const routes: Routes = [
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
-export class PlanetsPageRoutingModule {}
+export class StarshipsPageRoutingModule {}
